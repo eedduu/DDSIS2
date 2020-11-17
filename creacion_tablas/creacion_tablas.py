ed0b8a55015b2f928f0eaff8e66effb87021cd60
Carlos Lara <carloslaracasanova@gmail.com>
2020-11-17 15:57:27 +0100

    Borrado y creación de tablas.

--- creacion_tablas/creacion_tablas.py
+++ creacion_tablas/creacion_tablas.py
@@ -5,14 +5,25 @@ import pyodbc
 #############			Borrado y creación de tablas				   ###################
 ##################################################################################
 ##################################################################################
+#
+#	Documentación de pyodbc: https://github.com/mkleehammer/pyodbc/wiki/Objects
+#
+
 
 print("\nBorrado y creación de la base de las tablas...\n")
 
+# En UserID y Password cambiad los valores por los de vuestro DNI cuando lo useis
 print("Conectando a la base de datos...")
+
 conexion = pyodbc.connect('DRIVER={Devart ODBC Driver for Oracle};Direct=True;Host=oracle0;Service Name=practbd.oracle0.ugr.es;User ID=x7036964;Password=x7036964')
+
 print("Se ha conectado satisfactoriamente.\n")
 
+
+
 cursor = conexion.cursor()
+
+
 
 print("Borrando tablas previamente creadas...")
 
@@ -40,7 +51,9 @@ except:
 print("Fin de borrado de tablas.\n")
 
 
+
 print("Creando las tablas...")
+
 cursor.execute('''
 	CREATE TABLE Stock(
 		Cproducto int,
@@ -65,6 +78,7 @@ cursor.execute('''
 		FOREIGN KEY (Cpedido) REFERENCES Pedido(Cpedido),
 		FOREIGN KEY (Cproducto) REFERENCES Stock(Cproducto)
 	)''')
+
 print("Fin de creación de tablas.\n")
 
 conexion.commit()
