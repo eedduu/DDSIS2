fe935bb32b6ad06e2ebfcceb401dcee419c2e486
Edu Portatil <eedduu@github.com>
2020-11-19 21:34:46 +0100

    Actualizo

--- src/creacion_tablas.py
+++ src/creacion_tablas.py
@@ -10,12 +10,12 @@ import pyodbc
 #
 
 
-print("\nBorrado y creación de la base de las tablas...\n")
+print('Borrado y creación de la base de las tablas...')
 
 # En UserID y Password cambiad los valores por los de vuestro DNI cuando lo useis
 print("Conectando a la base de datos...")
 
-conexion = pyodbc.connect('DRIVER={Devart ODBC Driver for Oracle};Direct=True;Host=oracle0;Service Name=practbd.oracle0.ugr.es;User ID=x7036964;Password=x7036964')
+conexion = pyodbc.connect('DRIVER={Devart ODBC Driver for Oracle};Direct=True;Host=oracle0;Service Name=practbd.oracle0.ugr.es;User ID=x7200029;Password=x7200029')
 
 print("Se ha conectado satisfactoriamente.\n")
 
@@ -67,6 +67,7 @@ cursor.execute('''
 		PRIMARY KEY (Cpedido)
 	)''')
 
+
 cursor.execute('''
 	CREATE TABLE DetallePedido(
 		Cpedido int,
@@ -96,6 +97,43 @@ cursor.execute('''INSERT INTO Stock VALUES (10,1000)''')
 print("Fin de inserción de tuplas")
 
 conexion.commit()
+
+
+######
+###### PRUEBA CODIGO ELIMINAR Pedido
+######
+
+cursor.execute('''INSERT INTO Pedido VALUES (12345, 66666, 141)''')
+cursor.execute('''INSERT INTO Pedido VALUES (1234, 6666, 113)''')
+cursor.execute('''INSERT INTO DetallePedido VALUES (1234, 10, 5)''')
+
+cod_pedido=1234
+print('Borrando los detalles del pedido asociados al pedido: {}'.format(cod_pedido))
+
+try:
+	cursor.execute('''DELETE * FROM DetallePedido WHERE Cpedido=cod_pedido ''')
+except pyodbc.Error as error:
+	print('Error borrando las tuplas de la tabla DetallePedido:\n\t{}\n'.format(error))
+except:
+	print('Error no identificado borrando las tuplas de la tabla DetallePedido.')
+
+
+print('Borrando los pedidos con código: {}'.format(cod_pedido))
+
+
+try:
+	cursor.execute('''DELETE * FROM Pedido WHERE Cpedido=cod_pedido''')
+except pyodbc.Error as error:
+	print('Error borrando las tuplas de la tabla Pedidos:\n\t{}\n'.format(error))
+except:
+	print('Error no identificado borrando las tuplas de la tabla Pedidos.')
+
+print('Todos los cambios se han hecho correctamente, aplicando los cambios a la base de datos')
+conexion.commit()
+
+#####
+##### FIN PRUEBA
+#####
 
 print("Desconectandose de la bases de datos...")
 conexion.close()
