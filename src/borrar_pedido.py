e9bea5334eefe6f4874e3326e5961743ff293bf1
Edu Portatil <eedduu@github.com>
2020-11-20 12:26:27 +0100

    Termino borrar pedido

--- src/borrar_pedido.py
+++ src/borrar_pedido.py
@@ -1,21 +1,21 @@
 def borrar_pedido(conexion, cod_pedido):
     cursor= conexion.cursor()
 
-    print('Borrando los detalles del pedido asociados al pedido: {}'.cod_pedido)
+    print('Borrando los detalles del pedido asociados al pedido: {}'.format(cod_pedido))
 
     try:
-        cursor.execute('''DELETE * FROM DetallePedido WHERE Cpedido=cod_pedido ''')
+    	cursor.execute('''DELETE FROM DetallePedido WHERE Cpedido = {} '''.format(cod_pedido))
     except pyodbc.Error as error:
     	print('Error borrando las tuplas de la tabla DetallePedido:\n\t{}\n'.format(error))
     except:
     	print('Error no identificado borrando las tuplas de la tabla DetallePedido.')
 
 
-    print('Borrando los pedidos con código: {}'.cod_pedido)
+    print('Borrando los pedidos con código: {}'.format(cod_pedido))
 
 
     try:
-        cursor.execute('''DELETE * FROM Pedidos WHERE Cpedido=cod_pedido''')
+    	cursor.execute('''DELETE FROM Pedido WHERE Cpedido={}'''.format(cod_pedido))
     except pyodbc.Error as error:
     	print('Error borrando las tuplas de la tabla Pedidos:\n\t{}\n'.format(error))
     except:
--- src/creacion_tablas.py
+++ src/creacion_tablas.py
@@ -63,7 +63,7 @@ cursor.execute('''
 	CREATE TABLE Pedido(
 		Cpedido int,
 		Ccliente int,
-		FechaPedido date,
+		FechaPedido int,
 		PRIMARY KEY (Cpedido)
 	)''')
 
@@ -103,15 +103,21 @@ conexion.commit()
 ###### PRUEBA CODIGO ELIMINAR Pedido
 ######
 
-cursor.execute('''INSERT INTO Pedido VALUES (12345, 66666, 141)''')
-cursor.execute('''INSERT INTO Pedido VALUES (1234, 6666, 113)''')
-cursor.execute('''INSERT INTO DetallePedido VALUES (1234, 10, 5)''')
+try:
+	cursor.execute('''INSERT INTO Pedido VALUES (12345, 66666, 141)''')
+	cursor.execute('''INSERT INTO Pedido VALUES (1234, 6666, 113)''')
+	cursor.execute('''INSERT INTO DetallePedido VALUES (1234, 10, 5)''')
+except pyodbc.Error as error:
+	print('Error insertando tuplas de la tabla DetallePedido:\n\t{}\n'.format(error))
+except:
+	print('Error no identificado insertando las tuplas de la tabla DetallePedido.')
+
 
 cod_pedido=1234
 print('Borrando los detalles del pedido asociados al pedido: {}'.format(cod_pedido))
 
 try:
-	cursor.execute('''DELETE * FROM DetallePedido WHERE Cpedido=cod_pedido ''')
+	cursor.execute('''DELETE FROM DetallePedido WHERE Cpedido = {} '''.format(cod_pedido))
 except pyodbc.Error as error:
 	print('Error borrando las tuplas de la tabla DetallePedido:\n\t{}\n'.format(error))
 except:
@@ -122,7 +128,7 @@ print('Borrando los pedidos con código: {}'.format(cod_pedido))
 
 
 try:
-	cursor.execute('''DELETE * FROM Pedido WHERE Cpedido=cod_pedido''')
+	cursor.execute('''DELETE FROM Pedido WHERE Cpedido={}'''.format(cod_pedido))
 except pyodbc.Error as error:
 	print('Error borrando las tuplas de la tabla Pedidos:\n\t{}\n'.format(error))
 except:
