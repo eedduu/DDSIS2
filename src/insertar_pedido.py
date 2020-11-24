af6fc80716ca417bbd3064f2a2271cb94b5a71e4
luistf24 <luis.tormo24@gmail.com>
2020-11-24 11:21:37 +0100

    Añado insertar pedido modificado para dar detalle del pedido

--- src/insertar_pedido.py
+++ src/insertar_pedido.py
@@ -12,8 +12,15 @@ print("Introduzca nuevo pedido:\n")
 print("Código del Pedido: ")
 cpedido = input()
 
+print ("Código del Producto: ")
+cproducto = input()
+
+
 print("Código del cliente: ")
 ccliente = input()
+
+print ("Cantidad de producto: ")
+cantidad = input()
 
 print("Fecha pédido (Formato númerico):\n")
 
@@ -27,11 +34,20 @@ anyo = input()
 fecha = datetime.datetime(int(anyo),int(mes),int(dia))
 
 try:
-    cursor.execute("insert into Pedido (Cpedido,Ccliente,FechaPedido) values(?,?,?)",(cpedido, ccliente,fecha))
+    cursor.execute("insert into Pedido (Cpedido,Ccliente,FechaPedido) values(?,?,?)",(int(cpedido),int(ccliente),fecha))
 except pyodbc.Error as error:
     print('Error insertando en la tabla Pedido:\n\t{}\n'.format(error))
 except:
     print('Error no identificado insertando el pedido')
+
+try:
+    cursor.execute("insert into DetallePedido (Cpedido,Cproducto,Cantidad) values(?,?,?)",(int(cpedido),int(cproducto),int(cantidad)))
+except pyodbc.Error as error:
+    print('Error insertando en la tabla DetallePedido:\n\t{}\n'.format(error))
+except:
+    print('Error no identificado insertando el detalle del pedido')
+
+
 
 print("Se ha introducido el pedido en la base de datos")
 conexion.commit()
